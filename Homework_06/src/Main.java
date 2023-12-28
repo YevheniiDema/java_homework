d3ce36e12d8859c2ab6e5c3637daa5029968ecf8
YevheniiDema <jekdema@gmail.com>
2023-12-29 00:06:07 +0100

    Add files via upload

---
+++ Homework_06/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        /*1. Создайте две переменные isEdekaOpen и isReweOpen,
+        значения которых зависят от того, открыты магазины или нет.
+    Реализует логический метод canBuy,  возвращающий boolean
+    Значение этой переменной должно быть true, если хотя бы один магазин открыт, иначе false.
+    Отобразите строку «Я могу купить еду, это ……» и значение.*/
+        boolean isEdekaOpen = false;
+        boolean isReweOpen = false;
+        boolean buying = cabBuy(isEdekaOpen, isReweOpen);
+        System.out.println("Я могу купить эту еду, это " + buying);
+
+
+
+    }
+    public static boolean cabBuy(boolean firstMarkt, boolean secondMarkt) {
+        return firstMarkt || secondMarkt;
+    }
+}
